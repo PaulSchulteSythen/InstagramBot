72b5ce3b926fcd71fa902c6346b3d47a85b936ff
PaulSchulteSythen <paulsythen@gmail.com>
2021-03-07 01:01:04 +0100

    Update Program

--- Update_Programm/update.py
+++ Update_Programm/update.py
@@ -7,7 +7,7 @@ path = os.getcwd().replace("\\", "/") + "/"
 
 # files to update
 config_files = [REPOSITORY+"config/like_mainpage_xpaths.conf", REPOSITORY+"config/like_xpaths.conf", REPOSITORY+"config/save_xpaths.conf", REPOSITORY+"config/xpaths.conf"]
-exe_file = [REPOSITORY+"dist/GUI.exe"]
+exe_file = "https://github.com/PaulSchulteSythen/InstagramBot/raw/master/dist/GUI.exe"
 
 
 import requests
@@ -22,6 +22,9 @@ try:
 	# replace .exe file
 	r = requests.get(exe_file, allow_redirects=True)
 	open(path + "InstagramBot.exe", 'wb').write(r.content)
+
+	# success
+	ctypes.windll.user32.MessageBoxW(0, u"Das Update wurlde erfolgreich durchgeführt.", u"Update abgeschlossen", 0x40)
 except Exception as e:
 	print(e)
-	ctypes.windll.user32.MessageBoxW(None, u"Das Update hat nicht funktioniert. Überprüfen Sie die Internetverbindung oder ob das Verzeichnis schreibgeschützt ist.", u"Fehler", 0)
+	ctypes.windll.user32.MessageBoxW(0, u"Das Update hat nicht funktioniert. Überprüfen Sie die Internetverbindung oder ob das Verzeichnis schreibgeschützt ist.", u"Fehler", 0x10)
